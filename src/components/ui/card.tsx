276493eecc3ca2bf64abd7ece8ed839e46dd244c
Cyse95 <cysebaert@hotmail.fr>
2025-10-12 14:37:51 +0200

    Create card.tsx

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+type DivProps = React.HTMLAttributes<HTMLDivElement>;
+
+export function Card({ className, ...props }: DivProps) {
+  return <div className={className} {...props} />;
+}
+
+export function CardHeader({ className, ...props }: DivProps) {
+  return <div className={className} {...props} />;
+}
+
+export function CardTitle({ className, ...props }: DivProps) {
+  return <h3 className={className} {...props} />;
+}
+
+export function CardDescription({ className, ...props }: DivProps) {
+  return <p className={className} {...props} />;
+}
+
+export function CardContent({ className, ...props }: DivProps) {
+  return <div className={className} {...props} />;
+}
+
+export function CardFooter({ className, ...props }: DivProps) {
+  return <div className={className} {...props} />;
+}
